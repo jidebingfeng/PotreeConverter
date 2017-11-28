227d08c3ca7cc99f630da247614a3452b6a98c24
cnksi\zlong <jidebingfeng@126.com>
2017-11-28 11:31:00 +0800

    add compile shell

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#gcc version 5.4.1 20160904 (Ubuntu 5.4.1-2ubuntu1~14.04)
+mkdir ~/dev/workspaces/ -p
+cd ~/dev/workspaces/
+git clone https://github.com/jidebingfeng/PotreeConverter.git
+make
+
+
+sudo ln -s /home/zlong/dev/workspaces/PotreeConverter/build/PotreeConverter/PotreeConverter /usr/bin/PotreeConverterll
+
+
+
+
+
+cp ~/dev/workspaces/PotreeConverter/master/PotreeConverter/resources /path/to/workspace
